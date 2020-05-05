2430a0324a34bc5828e8ec0542898d11475772cb
jadedtdt <genebbb333@aol.com>
2020-05-05 18:56:18 -0400

    Create getusers.py

---
+++ getusers.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    
+    try:
+        body = client.scan(
+            TableName='MelvorHiscores'
+        )
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps(body)
+        }
+    except:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': None
+        }
